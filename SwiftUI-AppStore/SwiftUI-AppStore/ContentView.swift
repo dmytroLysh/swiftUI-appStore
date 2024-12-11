8b16f39792bc7b63b5d75a3d52482712d2575195
Dmytro Lyshtva <lyshtva.dmytro+github@gmail.com>
2024-12-11 23:08:56 +0100

    Global: initial commit

---
+++ SwiftUI-AppStore/SwiftUI-AppStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-AppStore
+//
+//  Created by Dmytro Lyshtva on 11.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-AppStore/SwiftUI-AppStore/SwiftUI_AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppStoreApp.swift
+//  SwiftUI-AppStore
+//
+//  Created by Dmytro Lyshtva on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
