84ab45aa597d815baa21836b8f14eaa6c8b86c11
Dmytro Lyshtva <lyshtva.dmytro+github@gmail.com>
2024-12-29 14:12:46 +0100

    (Main): Create custom UI for mock

--- SwiftUI-AppStore/SwiftUI-AppStore/ContentView.swift
+++ SwiftUI-AppStore/SwiftUI-AppStore/ContentView.swift
@@ -9,16 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            GeometryReader { proxy in
+                ScrollView {
+                    ForEach(0..<10) { num in
+                        VStack(spacing: 16) {
+                            
+                            AppIconTiitleView()
+                            
+                            ScreenshotsRow(proxy: proxy)
+                        }
+                        .padding(16)
+                    }
+                }
+                .navigationTitle("Search")
+                .searchable(text: .constant("Enter search term"))
+            }
         }
-        .padding()
+    }
+}
+
+struct AppIconTiitleView: View {
+    var body: some View {
+        HStack(spacing: 16) {
+            RoundedRectangle(cornerRadius: 16)
+                .frame(width: 80,height: 80)
+            VStack(alignment: .leading) {
+                Text("Youtube: Watch, Listen, blah blah blah")
+                    .lineLimit(1)
+                Text("Photo & Video")
+                    .foregroundStyle(.gray)
+                Text("STARS 34.0M")
+            }
+            Image(systemName: "icloud.and.arrow.down")
+        }
+    }
+}
+
+struct ScreenshotsRow: View {
+    let proxy: GeometryProxy
+    var body: some View {
+        let width = (proxy.size.width - 4 * 16) / 3
+        HStack(spacing: 16) {
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width: width,height: 200)
+            
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width: width,height: 200)
+            
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width: width,height: 200)
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
