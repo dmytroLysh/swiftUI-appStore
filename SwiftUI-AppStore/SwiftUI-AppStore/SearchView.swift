f3a1019bbabd6238feef038f9516e1d505e6afc0
Dmytro Lyshtva <lyshtva.dmytro+github@gmail.com>
2025-02-20 16:00:00 +0100

    Searchable with sink and debounce

--- SwiftUI-AppStore/SwiftUI-AppStore/SearchView.swift
+++ SwiftUI-AppStore/SwiftUI-AppStore/SearchView.swift
@@ -26,26 +26,39 @@ struct AppResults: Codable, Identifiable {
     let artistName: String
 }
 
+import Combine
+
 @MainActor
 class SearchViewModel: ObservableObject {
     
     @Published var results: [AppResults] = [AppResults]()
+    @Published var query = ""
+    
+    private var cancellables = Set<AnyCancellable>()
     
     init() {
-        fetchJsonData()
+        $query
+            .debounce(for: 0.5, scheduler: DispatchQueue.main)
+            .sink { [weak self] newValue in
+                
+                guard let self = self else { return }
+                
+                self.fetchJsonData(searchValue: newValue)
+            }.store(in: &cancellables)
+        
     }
     
-    private func fetchJsonData() {
+    private func fetchJsonData(searchValue: String) {
         Task {
             do {
-                guard let url = URL(string: "https://itunes.apple.com/search?term=tinder&entity=software") else { return }
+                guard let url = URL(string: "https://itunes.apple.com/search?term=\(searchValue)&entity=software") else { return }
                 let (data,_) = try await URLSession.shared.data(from: url)
                 
                 let searchResult = try JSONDecoder().decode(SearchResults.self, from: data)
                 
-                    self.results = searchResult.results
+                self.results = searchResult.results
                 
-
+                
             } catch {
                 print("Failed to due error", error)
             }
@@ -61,19 +74,34 @@ struct SearchView: View {
     var body: some View {
         NavigationStack {
             GeometryReader { proxy in
-                ScrollView {
-                    ForEach(vm.results) { result in
-                        VStack(spacing: 16) {
-                            
-                            AppIconTiitleView(result: result)
-                            ScreenshotsRow(proxy: proxy,results: result )
+                ZStack {
+                    
+                    if vm.results.isEmpty && vm.query.isEmpty {
+                        VStack (spacing: 16){
+                            Image(systemName: "magnifyingglass.circle.fill")
+                                .font(.system(size: 60))
+                            Text("Please enter your search term above")
+                                .font(.system(size: 24,weight: .semibold))
+                                .multilineTextAlignment(.center)
                         }
-                        .padding(16)
+                        .frame(maxWidth: .infinity,maxHeight: .infinity)
+                        
+                    } else {
+                        ScrollView {
+                            ForEach(vm.results) { result in
+                                VStack(spacing: 16) {
+                                    
+                                    AppIconTiitleView(result: result)
+                                    ScreenshotsRow(proxy: proxy,results: result)
+                                }
+                                .padding(16)
+                            }
+                        }
                     }
                 }
-                .navigationTitle("Search")
-                .searchable(text: .constant("Enter search term"))
             }
+            .navigationTitle("Search")
+            .searchable(text: $vm.query)
         }
     }
 }
@@ -95,7 +123,7 @@ struct AppIconTiitleView: View {
                 RoundedRectangle(cornerRadius: 16)
                     .frame(width: 80,height: 80)
             }
-
+            
             VStack(alignment: .leading) {
                 Text(result.trackName)
                     .lineLimit(1)
@@ -128,7 +156,7 @@ struct ScreenshotsRow: View {
                     RoundedRectangle(cornerRadius: 12)
                         .frame(width: width ,height: 200)
                 }
-
+                
             }
         }
     }
